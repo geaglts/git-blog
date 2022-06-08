c0618e625ea6aa15905703f95aaacb0045f3a407
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-06-07 19:36:08 -0500

    Social media icons in the footer

---
+++ src/catalogs/socialMedias.ts
@@ -0,0 +1,31 @@
+import github from '@icons/github.svg';
+import gxlts from '@icons/gxlts.svg';
+import twitter from '@icons/twitter.svg';
+import linkedin from '@icons/linkedin.svg';
+
+export default [
+    {
+        key: 1,
+        image: linkedin,
+        to: 'https://www.linkedin.com/in/geaglts',
+        alt: 'link to geaglts linkedin profile',
+    },
+    {
+        key: 2,
+        image: github,
+        to: 'https://github.com/geaglts',
+        alt: 'link to geaglts github profile',
+    },
+    {
+        key: 3,
+        image: twitter,
+        to: 'https://twitter.com/GxltsDev',
+        alt: 'link to geaglts twitter profile',
+    },
+    {
+        key: 4,
+        image: gxlts,
+        to: 'https://gxlts.tk',
+        alt: 'link to geaglts web page',
+    },
+];
--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
 import styles from '@styles/Footer.module.scss';
+
+import socialMedias from 'catalogs/socialMedias';
 
 const Footer = () => {
     return (
@@ -6,6 +9,19 @@ const Footer = () => {
             <p className={styles.Author}>
                 @gxlts &bull; Miguel Angel Toledo Santiago ❤️ &bull; &copy;
             </p>
+            <div className={styles.SocialLinkContainer}>
+                {socialMedias.map(({ image, alt, key, to }) => (
+                    <a
+                        key={key}
+                        href={to}
+                        target={'_blank'}
+                        className={styles.SocialLink}
+                        title={alt}
+                    >
+                        <Image src={image} alt={alt} />
+                    </a>
+                ))}
+            </div>
         </footer>
     );
 };
